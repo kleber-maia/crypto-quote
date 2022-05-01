7de089332831610133da7977d54183bd7ce5a80e
Kleber Maia <talkto@klebermaia.com>
2022-05-01 17:06:40 -0400

    Initial Commit

---
+++ Crypto Quote/Crypto_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_QuoteApp.swift
+//  Crypto Quote
+//
+//  Created by Kleber on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
